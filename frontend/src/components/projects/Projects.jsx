d42961fd9e791dbe9793eab4e9017b4641b14046
kowshikramtg <kowshik1618@gmail.com>
2026-06-14 22:52:49 +0530

    updated projects

--- frontend/src/components/projects/Projects.jsx
+++ frontend/src/components/projects/Projects.jsx
@@ -1,7 +1,5 @@
 import "./projects.css";
-import project1 from "../../assets/project1.png";
-import project2 from "../../assets/project2.png";
-import project3 from "../../assets/project3.png";
+import { projectsData } from "./projectsData";
 
 function Project() {
   return (
@@ -9,166 +7,47 @@ function Project() {
       <div className="projects">
         <h2 className="ProjTitle">Projects</h2>
         <div className="projectsList">
-          <div className="projectItem firstItem">
+          {
+            projectsData.map((project, index) => (
+              <div
+                key={index}
+                className={`projectItem ${project.className || ""}`}
+              >
                 <a
-                  href="https://sudhaherbalbeautyparlour.vercel.app/"
+                  href={project.link}
                   target="_blank"
                   rel="noopener noreferrer"
                   className="project-link"
                 >
-                  <h3>Sudha Herbal Beauty Parlour</h3>
+                  <h3>{project.title}</h3>
+
                   <div className="project-image-wrapper">
                     <img
-                      src={project3}
-                      alt="parlor image"
+                      src={project.image}
+                      alt={project.alt}
                       className="project-img"
                     />
+
                     <div className="tech-overlay">
-                      <div className="tech">
-                        <span>MERN</span>
-                        <div className="bar">
-                          <div style={{ width: "90%" }}></div>
+                      {project.techs.map((tech, i) => (
+                        <div className="tech" key={i}>
+                          <span>{tech.name}</span>
+
+                          <div className="bar">
+                            <div style={{ width: tech.width }}></div>
+                          </div>
+
+                          <span className="percent">
+                            {tech.percent}
+                          </span>
                         </div>
-                        <span className="percent">90%</span>
-                      </div>
-                      <div className="tech">
-                        <span>CSS</span>
-                        <div className="bar">
-                          <div style={{ width: "6%" }}></div>
-                        </div>
-                        <span className="percent">6%</span>
-                      </div>
-                      <div className="tech">
-                        <span>API</span>
-                        <div className="bar">
-                          <div style={{ width: "4%" }}></div>
-                        </div>
-                        <span className="percent">4%</span>
-                      </div>
+                      ))}
                     </div>
                   </div>
                 </a>
-          </div>
-          <div className="projectItem">
-            <a
-              href="https://attendance-calculator-six.vercel.app/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="project-link"
-            >
-              <h3>Attendance-Bunk Calculator</h3>
-              <div className="project-image-wrapper">
-                <img
-                  src={project1}
-                  alt="website Dashboard"
-                  className="project-img"
-                />
-
-                <div className="tech-overlay">
-                  <div className="tech">
-                    <span>JS</span>
-                    <div className="bar">
-                      <div style={{ width: "70%" }}></div>
-                    </div>
-                    <span className="percent">65%</span>
-                  </div>
-                  <div className="tech">
-                    <span>CSS</span>
-                    <div className="bar">
-                      <div style={{ width: "20%" }}></div>
-                    </div>
-                    <span className="percent">28%</span>
-                  </div>
-                  <div className="tech">
-                    <span>HTML</span>
-                    <div className="bar">
-                      <div style={{ width: "10%" }}></div>
-                    </div>
-                    <span className="percent">7%</span>
-                  </div>
-                </div>
               </div>
-            </a>
-          </div>
-          <div className="projectItem center-big">
-            <a
-              href="https://dad-joke-generate.vercel.app/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="project-link"
-            >
-              <h3>Dad Joke Generate</h3>
-              <div className="project-image-wrapper">
-                <img
-                  src={project2}
-                  alt="Dad joke Generate"
-                  className="project-img"
-                />
-                <div className="tech-overlay">
-                  <div className="tech">
-                    <span>HTML</span>
-                    <div className="bar">
-                      <div style={{ width: "70%" }}></div>
-                    </div>
-                    <span className="percent">76%</span>
-                  </div>
-                  <div className="tech">
-                    <span>JS</span>
-                    <div className="bar">
-                      <div style={{ width: "20%" }}></div>
-                    </div>
-                    <span className="percent">19%</span>
-                  </div>
-                  <div className="tech">
-                    <span>API</span>
-                    <div className="bar">
-                      <div style={{ width: "10%" }}></div>
-                    </div>
-                    <span className="percent">5%</span>
-                  </div>
-                </div>
-              </div>
-            </a>
-          </div>
-          <div className="projectItem right-bottom">
-            <a
-              href=""
-              target="_blank"
-              rel="noopener noreferrer"
-              className="imgs"
-            >
-              <h3>on work</h3>
-              <div className="imgs">
-                <img src="" alt="project2" />
-              </div>
-            </a>
-          </div>
-          {/* <div className="projectItem center-small">
-            <a
-              href=""
-              target="_blank"
-              rel="noopener noreferrer"
-              className="imgs"
-            >
-              <h3>Project 4</h3>
-              <div className="imgs">
-                <img src="" alt="" />
-              </div>
-            </a>
-          </div>
-          <div className="projectItem right-top">
-            <a
-              href=""
-              target="_blank"
-              rel="noopener noreferrer"
-              className="imgs"
-            >
-              <h3>Project 5</h3>
-              <div className="imgs">
-                <img src="" alt="" />
-              </div>
-            </a>
-          </div> */}
+            ))
+          }
         </div>
       </div>
     </>
---
+++ frontend/src/components/projects/projectsData.js
@@ -0,0 +1,76 @@
+import project1 from "../../assets/project1.png";
+import project2 from "../../assets/project2.png";
+import project3 from "../../assets/project3.png";
+import project4 from "../../assets/project4.png";
+import project5 from "../../assets/project5.png";
+
+export const projectsData = [
+    // TypeIt
+  {
+    title: "TypeIt",
+    link: "https://typit-seven.vercel.app/",
+    image: project4,
+    alt: "TypeIt",
+    techs: [
+      { name: "React", width: "45%", percent: "45%" },
+      { name: "TS", width: "25%", percent: "25%" },
+      { name: "Firebase", width: "20%", percent: "20%" },
+      { name: "Tailwind", width: "10%", percent: "10%" }
+    ]
+  },
+
+  // PowerPredict
+  {
+    title: "PowerPredict",
+    link: "https://powerpredict.vercel.app/",
+    image: project5,
+    alt: "PowerPredict",
+    techs: [
+      { name: "React", width: "40%", percent: "40%" },
+      { name: "Flask", width: "30%", percent: "30%" },
+      { name: "PyTorch", width: "15%", percent: "15%" },
+      { name: "Websocket", width: "10%", percent: "10%" },
+      { name: "TailwindCSS", width: "5%", percent: "5%" },
+      { name: "Kaggle", width: "", percent: "" }
+    ]
+  },
+  {
+    title: "Sudha Herbal Beauty Parlour",
+    link: "https://sudhaherbalbeautyparlour.vercel.app/",
+    image: project3,
+    alt: "parlor image",
+    className: "firstItem",
+    techs: [
+      { name: "MERN", width: "90%", percent: "90%" },
+      { name: "CSS", width: "6%", percent: "6%" },
+      { name: "API", width: "4%", percent: "4%" }
+    ]
+  },
+
+  {
+    title: "Attendance-Bunk Calculator",
+    link: "https://attendance-calculator-six.vercel.app/",
+    image: project1,
+    alt: "website Dashboard",
+    techs: [
+      { name: "JS", width: "70%", percent: "65%" },
+      { name: "CSS", width: "20%", percent: "28%" },
+      { name: "HTML", width: "10%", percent: "7%" }
+    ]
+  },
+
+  {
+    title: "Dad Joke Generate",
+    link: "https://dad-joke-generate.vercel.app/",
+    image: project2,
+    alt: "Dad joke Generate",
+    className: "center-big",
+    techs: [
+      { name: "HTML", width: "70%", percent: "76%" },
+      { name: "JS", width: "20%", percent: "19%" },
+      { name: "API", width: "10%", percent: "5%" }
+    ]
+  },
+
+  
+];
